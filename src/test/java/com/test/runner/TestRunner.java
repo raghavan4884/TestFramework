25f9969e68be39fb433b3c0158983479897206eb
raghavan4884 <86198055+raghavan4884@users.noreply.github.com>
2025-03-08 19:23:03 +0530

    Update TestRunner.java

--- src/test/java/com/test/runner/TestRunner.java
+++ src/test/java/com/test/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features",glue= {"com.test.stepdefns"},monochrome=true,dryRun=false,plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+@CucumberOptions(features="src/test/resources/Features",glue= {"com.test.stepdefns"},monochrome=true,dryRun=false,plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","json:target/reports.json"})
 public class TestRunner {
 
 }
