f58e57f87e5e618209e1c4c620a2ef1835f5330c
raghavan <raghavan4884@gmail.com>
2025-03-08 18:02:01 +0530

    fifth

---
+++ src/test/java/com/test/stepdefns/StepDefns.java
@@ -0,0 +1,26 @@
+package com.test.stepdefns;
+
+import io.cucumber.java.en.Given;
+
+public class StepDefns {
+
+	@Given("that  the value")
+	public void that_the_user_enters_the_value() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("hello world");
+		System.out.println("world");
+		System.out.println("dfd");
+		System.out.println("wrerettt");
+		System.out.println("wre");
+		System.out.println("wreeeere");
+		System.out.println("weerrt");
+		System.out.println("fsfsfsfsdfds");
+		System.out.println("dddd");
+		System.out.println("eeeee");
+		System.out.println("yyyyyy");
+		System.out.println("gofor");
+		System.out.println("sdfsdfsdfsf");
+		System.out.println("sfdsfsdfsdfsdfsdfsdfsdfdsf");
+
+}
+}
