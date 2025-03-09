0ba41b3dccadb92a753d3cbc19b1c190858454f8
raghavan <raghavan4884@gmail.com>
2025-03-09 13:06:17 +0530

    fourth

--- src/test/java/com/test/runner/TestRunner.java
+++ src/test/java/com/test/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features",glue= {"com.test.stepdefns"},monochrome=true,dryRun=false,plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+@CucumberOptions(features="src/test/resources/Features",glue= {"com.test.stepdefns"},monochrome=true,dryRun=false,plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","html:target/reports.html"})
 public class TestRunner {
 
 }
