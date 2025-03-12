26857967a4daada89d78de3d49c81a50a5d772d7
raghavan4884 <86198055+raghavan4884@users.noreply.github.com>
2025-03-12 23:04:05 +0530

    Update StepDefinitions.java

--- src/test/java/com/test/stepdefns/StepDefinitions.java
+++ src/test/java/com/test/stepdefns/StepDefinitions.java
@@ -35,7 +35,7 @@ public class StepDefinitions {
 	@Given("that the user enters the values")
 	public void that_the_user_enters_the_values() {
 	    // Write code here that turns the phrase above into concrete actions
-		String browser=System.getProperty("browser");
+		String browser=System.getEnv("browser");
 		System.out.println(browser);
 		System.out.println("water5");
 	}
