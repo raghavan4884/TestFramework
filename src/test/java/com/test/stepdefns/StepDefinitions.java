a9a46480f09e03b9ec1a2b087dc4eb5109cfbabd
raghavan4884 <86198055+raghavan4884@users.noreply.github.com>
2025-03-12 20:46:03 +0530

    Update StepDefinitions.java

--- src/test/java/com/test/stepdefns/StepDefinitions.java
+++ src/test/java/com/test/stepdefns/StepDefinitions.java
@@ -33,7 +33,7 @@ public class StepDefinitions {
 
 
 	@Given("that the user enters the values")
-	public void that_the_user_enters_the_values() {
+	public void that_the_user_enters_the_values(String s) {
 	    // Write code here that turns the phrase above into concrete actions
 		
 		System.out.println("water5");
